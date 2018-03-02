4b733284f8bca5c06b3f08893c96a7a601f85341
vlarok <vladimir.rokovanov@gmail.com>
2018-03-02 10:26:23 +0200

    init

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+
+//stateful component
+class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Burger ingredients = {this.state.ingredients} />
+                <div>Burger content</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
