6a88a84b085f90aaa1ada85844dcc2df313d2ff5
vlarok <vladimir.rokovanov@gmail.com>
2018-03-04 20:33:22 +0200

    order button and style

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,7 @@ const buildControls = (props) => (
                 disabled = {props.disabled[ctrl.type]}
             />
         ))}
+        <button className={classes.OrderButton}>ORDER NOW</button>
     </div> 
 );
 
