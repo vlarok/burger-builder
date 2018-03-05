36dd2fafe33fc92d5db2566374df51b12cf2bc36
vlarok <vladimir.rokovanov@gmail.com>
2018-03-05 23:03:21 +0200

    restructure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-
+import Modal from '../../components/UI/Modal/Modal';
 //stateful component
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -74,6 +74,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal />
                 <Burger ingredients = {this.state.ingredients} />
                 <BuildControls
                     ingredientAdded = {this.addIgredientHandler}
