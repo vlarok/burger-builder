bc04ee2fdfb4500c0ed544b3e80e760a769fa2bd
vlarok <vladimir.rokovanov@gmail.com>
2018-03-06 23:24:01 +0200

    sideDraw, logo size resizing

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Logo.css'
 import burgerLogo from '../../assets/images/burger-logo.png';
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.heigth}}>
         <img src = {burgerLogo} alt="BurgerBuilder" />
     </div>
 );
